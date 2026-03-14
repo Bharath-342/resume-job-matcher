7641e9fff2c562e8576b03b8cd57a16143bc3f65
Amar7564 <amarsatishmandalapu20@gmail.com>
2026-03-14 18:06:04 +0530

    again checking

--- server.js
+++ server.js
@@ -11,10 +11,21 @@ app.use(cors());
 app.use(express.json());
 
 
+app.get("/", (req, res) => {
+  res.send("Resume Job Matcher API is running");
+});
 app.use("/api", matchRoute);
 
 const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+});
+
+
+const pool = require("./config/db");
+
+pool.query("SELECT NOW()", (err, res) => {
+  if (err) console.log("DB ERROR", err);
+  else console.log("DB Connected:", res.rows);
 });
