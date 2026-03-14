58e5af05f39b83d8487185b27d4885ad9fd9c0fa
Amar7564 <amarsatishmandalapu20@gmail.com>
2026-03-14 17:55:27 +0530

    updated

--- config/db.js
+++ config/db.js
@@ -1,11 +1,14 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: "postgres",
-  host: "host.docker.internal",
-  database: "resume_matcher",
-  password: "post",
-  port: 5432
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
+
+pool.connect()
+  .then(() => console.log("PostgreSQL connected"))
+  .catch(err => console.error("PostgreSQL connection error:", err));
 
 module.exports = pool;
